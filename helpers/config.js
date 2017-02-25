da2b35c13a29b62d2c6f61bc977fda9b05d1ef53
Nnutthowze <briareibismuth@gmail.com>
2017-02-25 17:31:09 +0200

    Renamed constants to config

---
+++ helpers/config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const DB_TIMER = 30000; // represents 30 seconds equals 30000 (30 * 1000) milliseconds
+const REF_TIMER = 60000; // represents 60 seconds equals 60000 (60 * 1000) milliseconds
+const DB_PATH = path.join(__dirname, '../../../local_data/db');
+const REFERRER_FILE = path.join(DB_PATH, '../referrer.json');
+
+module.exports = {
+  DB_TIMER,
+  REF_TIMER,
+  DB_PATH,
+  REFERRER_FILE,
+};
--- helpers/constants.js
+++
@@ -1,4 +0,0 @@
-const DB_TIMER = 30000; // represents 30 seconds equals 30000 (30 * 1000) milliseconds
-const REF_TIMER = 60000; // represents 60 seconds equals 60000 (60 * 1000) milliseconds
-
-module.exports = { DB_TIMER, REF_TIMER };
